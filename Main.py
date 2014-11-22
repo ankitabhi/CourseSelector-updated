03c8315889eb83cafe789d7609857a8ae537bda4
praj24 <psarolker@gmail.com>
2014-11-22 09:42:41 -0500

    Added login code

--- Main.py
+++ Main.py
@@ -1,7 +1,10 @@
 __author__ = 'pa'
 
-from flask import request, Flask, render_template, jsonify
+from flask import Flask, render_template,request,session,flash,redirect,url_for
 
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'development key'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -14,6 +17,23 @@ def home_page():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    print "in login"
+    error = None
+    if request.method == 'POST':
+        if request.form['login'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return render_template("userHomePage.html")
+    return render_template('index.html', error=error)
+
+
 
 if __name__ == '__main__':
     app.run()
