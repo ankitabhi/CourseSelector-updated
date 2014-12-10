1a57300d3c2f9633721399d6bc00a702a7f96d42
praj24 <psarolker@gmail.com>
2014-12-10 14:18:35 -0500

    added check for if pre req taken

--- register.py
+++ register.py
@@ -115,9 +115,41 @@ class Register:
         query = "select c.course_code,c.course_name,c.prerequisite,c.credits,c.description from course_lookup c where c.course_code in (SELECT t.course_code from course_tags t where t.tag_name IN (SELECT s.tag_name from student_interests s where s.s_id = "+str(id)+"))and c.course_code not in (SELECT cc.course_code from student_course_details cc where cc.id ="+str(id)+")"
         cursor = conn.execute(query)
         entries = [dict(code=row[0], name=row[1],prereq=row[2],credits=row[3],desc=row[4]) for row in cursor.fetchall()]
+
+        query = "select c.prerequisite from course_lookup c where c.course_code in (SELECT t.course_code from course_tags t where t.tag_name IN (SELECT s.tag_name from student_interests s where s.s_id = "+str(id)+"))and c.course_code not in (SELECT cc.course_code from student_course_details cc where cc.id ="+str(id)+")  "
+        cursor = conn.execute(query)
+        entries1 = [dict(prereq=row[0]) for row in cursor.fetchall()]
+        #print "here"
+
+
+        codeList = [0 for x in range(len(entries1))]
+        for i in range(0,len(entries1)):
+
+            d = entries1[i]
+            for j in range(len(d)):
+
+                code = d['prereq']
+                cursor = conn.execute('select * from student_course_details where course_code ="'+str(code)+'"')
+                cur = cursor.fetchall()
+                if cur.__len__()>0:
+                    codeList[i] = "yes"
+                else:
+                    if code == "None":
+                        codeList[i] = "-"
+                    else:
+                        codeList[i]="no"
+
+
+        #print codeList
+        for d,num in zip(entries,codeList):
+            d['taken'] = num
+
+        #print entries
+
         return entries
 
     def getCoursesBasedOnTaken(self,user):
+        print "in getCoursesBasedOnTaken"
         cursor = conn.execute('select id from student_account_details where email = "'+user+'"')
         cur = cursor.fetchone()
         id =  cur[0]
@@ -125,4 +157,66 @@ class Register:
         query = "select cc.course_code,cc.course_name,cc.prerequisite,cc.credits,cc.description from course_lookup cc,course_tags tt where cc.course_code = tt.course_code and tt.tag_name in (select t.tag_name from course_tags t where t.course_code in (SELECT c.course_code from student_course_details c where c.id = "+str(id)+")) and cc.course_code not in (SELECT c.course_code from student_course_details c where c.id ="+str(id)+")"
         cursor = conn.execute(query)
         entries = [dict(code=row[0], name=row[1],prereq=row[2],credits=row[3],desc=row[4]) for row in cursor.fetchall()]
+        print entries
+
+        query = "select cc.prerequisite from course_lookup cc,course_tags tt where cc.course_code = tt.course_code and tt.tag_name in (select t.tag_name from course_tags t where t.course_code in (SELECT c.course_code from student_course_details c where c.id = "+str(id)+")) and cc.course_code not in (SELECT c.course_code from student_course_details c where c.id ="+str(id)+")"
+        cursor = conn.execute(query)
+        entries1 = [dict(prereq=row[0]) for row in cursor.fetchall()]
+        print "here"
+
+
+        codeList = [0 for x in range(len(entries1))]
+        for i in range(0,len(entries1)):
+
+            d = entries1[i]
+            for j in range(len(d)):
+
+                code = d['prereq']
+                print code
+                cursor = conn.execute('select * from student_course_details where course_code ="'+str(code)+'"')
+                cur = cursor.fetchall()
+                if cur.__len__()>0:
+                    codeList[i] = "yes"
+                else:
+                    if code == "None":
+                        codeList[i] = "-"
+                    else:
+                        codeList[i]="no"
+
+
+        print codeList
+        for d,num in zip(entries,codeList):
+            d['taken'] = num
+
+        print entries
+
         return entries
+
+    def checkIfPreReqTaken(self,user):
+        cursor = conn.execute('select id from student_account_details where email = "'+user+'"')
+        cur = cursor.fetchone()
+        id =  cur[0]
+
+        query = "select c.prerequisite from course_lookup c where c.course_code in (SELECT t.course_code from course_tags t where t.tag_name IN (SELECT s.tag_name from student_interests s where s.s_id = "+str(id)+"))and c.course_code not in (SELECT cc.course_code from student_course_details cc where cc.id ="+str(id)+")  "
+        cursor = conn.execute(query)
+        entries = [dict(prereq=row[0]) for row in cursor.fetchall()]
+        print entries
+        print str(len(entries))
+
+        result = []
+        for i in range(0,len(entries)):
+            print entries[i]
+            d = entries[i]
+            for j in range(len(d)):
+                print d['prereq']
+                code = d['prereq']
+                cursor = conn.execute('select * from student_course_details where course_code ="'+str(code)+'"')
+                cur = cursor.fetchall()
+                if cur.__len__()>0:
+                    result.append({"code":"yes"})
+                else:
+                    result.append({"code":"no"})
+            print result
+
+        print result
+        return result
