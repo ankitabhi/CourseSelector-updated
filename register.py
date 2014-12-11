c90df885e2e6d7f73c69b2e106c9baf3236c3756
praj24 <psarolker@gmail.com>
2014-12-10 23:31:55 -0500

    .
    .

--- register.py
+++ register.py
@@ -114,11 +114,11 @@ class Register:
         cur = cursor.fetchone()
         id =  cur[0]
 
-        query = "select c.course_code,c.course_name,c.prerequisite,c.credits,c.description from course_lookup c where c.course_code in (SELECT t.course_code from course_tags t where t.tag_name IN (SELECT s.tag_name from student_interests s where s.s_id = "+str(id)+"))and c.course_code not in (SELECT cc.course_code from student_course_details cc where cc.id ="+str(id)+")"
+        query = "select c.course_code,c.course_name,c.prerequisite,c.credits,c.description from course_lookup c where c.course_code in (SELECT t.course_code from course_tags t where t.tag_name IN (SELECT s.tag_name from student_interests s where s.s_id = "+str(id)+"))and c.course_code not in (SELECT cc.course_code from student_course_details cc where cc.id ="+str(id)+") group by c.course_code;"
         cursor = conn.execute(query)
         entries = [dict(code=row[0], name=row[1],prereq=row[2],credits=row[3],desc=row[4]) for row in cursor.fetchall()]
 
-        query = "select c.prerequisite from course_lookup c where c.course_code in (SELECT t.course_code from course_tags t where t.tag_name IN (SELECT s.tag_name from student_interests s where s.s_id = "+str(id)+"))and c.course_code not in (SELECT cc.course_code from student_course_details cc where cc.id ="+str(id)+")  "
+        query = "select c.prerequisite from course_lookup c where c.course_code in (SELECT t.course_code from course_tags t where t.tag_name IN (SELECT s.tag_name from student_interests s where s.s_id = "+str(id)+"))and c.course_code not in (SELECT cc.course_code from student_course_details cc where cc.id ="+str(id)+") group by c.course_code; "
         cursor = conn.execute(query)
         entries1 = [dict(prereq=row[0]) for row in cursor.fetchall()]
         #print "here"
@@ -156,12 +156,12 @@ class Register:
         cur = cursor.fetchone()
         id =  cur[0]
 
-        query = "select cc.course_code,cc.course_name,cc.prerequisite,cc.credits,cc.description from course_lookup cc,course_tags tt where cc.course_code = tt.course_code and tt.tag_name in (select t.tag_name from course_tags t where t.course_code in (SELECT c.course_code from student_course_details c where c.id = "+str(id)+")) and cc.course_code not in (SELECT c.course_code from student_course_details c where c.id ="+str(id)+")"
+        query = "select cc.course_code,cc.course_name,cc.prerequisite,cc.credits,cc.description from course_lookup cc,course_tags tt where cc.course_code = tt.course_code and tt.tag_name in (select t.tag_name from course_tags t where t.course_code in (SELECT c.course_code from student_course_details c where c.id = "+str(id)+")) and cc.course_code not in (SELECT c.course_code from student_course_details c where c.id ="+str(id)+") group by cc.course_code;"
         cursor = conn.execute(query)
         entries = [dict(code=row[0], name=row[1],prereq=row[2],credits=row[3],desc=row[4]) for row in cursor.fetchall()]
         print entries
 
-        query = "select cc.prerequisite from course_lookup cc,course_tags tt where cc.course_code = tt.course_code and tt.tag_name in (select t.tag_name from course_tags t where t.course_code in (SELECT c.course_code from student_course_details c where c.id = "+str(id)+")) and cc.course_code not in (SELECT c.course_code from student_course_details c where c.id ="+str(id)+")"
+        query = "select cc.prerequisite from course_lookup cc,course_tags tt where cc.course_code = tt.course_code and tt.tag_name in (select t.tag_name from course_tags t where t.course_code in (SELECT c.course_code from student_course_details c where c.id = "+str(id)+")) and cc.course_code not in (SELECT c.course_code from student_course_details c where c.id ="+str(id)+") group by cc.course_code;"
         cursor = conn.execute(query)
         entries1 = [dict(prereq=row[0]) for row in cursor.fetchall()]
         print "here"
